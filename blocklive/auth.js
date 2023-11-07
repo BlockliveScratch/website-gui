0152259b4cc282c917ef74ebd11a56d3335f62ca
micahpowch <powchm@catlin.edu>
2023-11-06 21:04:24 -0800

    feat: set demo username

--- blocklive/auth.js
+++ blocklive/auth.js
@@ -1,1 +1,3 @@
-let uname = 'samesame'
+let demoUsernames = ['aVerySexyScratchUser','rgantzos','ilhp10','scratch-cat']
+let uname = demoUsernames[Math.floor(Math.random()*demoUsernames.length)]+String(Math.floor(Math.random()*100))
+console.log(uname)
