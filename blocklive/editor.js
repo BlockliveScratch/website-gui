d94bf4e4e8c37075c4a3b146072ded6f39d8a69c
MeechaPooch <powchm@catlin.edu>
2024-06-28 07:45:38 -0700

    fix: incoherent blubbering

--- blocklive/editor.js
+++ blocklive/editor.js
@@ -3359,18 +3359,15 @@ async function addMessage(msg, notif) {
 
     msgsElem.scrollTop = msgsElem.scrollHeight;
 
-//? {
+
     if(notif) {
-//? }
         if(!isChatOpen()) {
             incChatUnread();
         }
-//? {
         if(!isChatOpen() || !document.hasFocus()) {
             liveMessage({meta:"chatnotif",project:store.getState().preview.projectInfo.title, sender:msg.sender, text:msg.text, avatar:(await getUserInfo(msg.sender)).pic})
         }
     }
-    //? }
 }
 function postMessageBubble() {
     let inputElem = document.querySelector('bl-chat-input')
